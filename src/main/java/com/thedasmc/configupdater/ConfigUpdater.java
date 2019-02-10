d35737aebf353a767a16249cf53fa49935cb13c2
tchristofferson <longleadtrc@gmail.com>
2019-02-10 14:16:14 -0600

    Update ConfigUpdater.java
    Allows updating from InputStream

--- src/main/java/com/thedasmc/configupdater/ConfigUpdater.java
+++ src/main/java/com/thedasmc/configupdater/ConfigUpdater.java
@@ -7,86 +7,132 @@ import java.io.*;
 
 public class ConfigUpdater {
 
+    /**
+     * Update a yml file from another yml file
+     * @param toUpdate The yml file to update
+     * @param updateFrom The yml file to update from
+     */
     public static void update(File toUpdate, File updateFrom) {
 
-        FileConfiguration config = YamlConfiguration.loadConfiguration(toUpdate);
+        try {
 
-        if (!toUpdate.exists()) {
-
-            YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(updateFrom);
-            try {
-                yamlConfiguration.save(toUpdate);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return;
-
-        }
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(updateFrom));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(toUpdate))) {
-
-            String line;
-            outer: while ((line = reader.readLine()) != null) {
-
-                if (line.startsWith("#")) {
-
-                    writer.write(line);
-                    writer.newLine();
-                    continue;
-
-                }
-
-                for (String key : config.getKeys(true)) {
-
-                    String[] keyArray = key.split("\\.");
-                    String keyString = keyArray[keyArray.length - 1];
-
-                    if (line.trim().startsWith(keyString + ":")) {
-
-                        if (config.isConfigurationSection(key)) {
-
-                            writer.write(line);
-                            writer.newLine();
-                            continue outer;
-
-                        }
-
-                        String[] array = line.split(": ");
-
-                        if (array.length == 2) {
-
-                            if (array[1].startsWith("\"") || array[1].startsWith("'")) {
-
-                                char c = array[1].charAt(0);
-                                line = array[0] + ": " + c + config.get(key) + c;
-
-                            } else {
-
-                                line = array[0] + ": " + config.get(key);
-
-                            }
-
-                        }
-
-                        writer.write(line);
-                        writer.newLine();
-                        continue outer;
-
-                    }
-
-                }
-
-                writer.write(line);
-                writer.newLine();
-
-            }
+            BufferedReader reader = getBufferedReader(updateFrom);
+            update(toUpdate, reader);
 
         } catch (IOException e) {
 
             e.printStackTrace();
 
         }
+
+    }
+
+    /**
+     * Update a yml file from an InputStream
+     * This is useful for default configs (config saved in jar) using {@link org.bukkit.plugin.java.JavaPlugin#getResource(String)}
+     * @param toUpdate The yml file to update
+     * @param updateFrom The InputStream to update from
+     */
+    public static void update(File toUpdate, InputStream updateFrom) {
+
+        BufferedReader reader = getBufferedReader(updateFrom);
+
+        try {
+
+            update(toUpdate, reader);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    private static void update(File toUpdate, BufferedReader reader) throws IOException {
+
+        FileConfiguration config = YamlConfiguration.loadConfiguration(toUpdate);
+
+        if (!toUpdate.exists()) {
+
+            config.save(toUpdate);
+            return;
+
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(toUpdate));
+
+        String line;
+        outer: while ((line = reader.readLine()) != null) {
+
+            if (line.startsWith("#")) {
+
+                writer.write(line);
+                writer.newLine();
+                continue;
+
+            }
+
+            for (String key : config.getKeys(true)) {
+
+                String[] keyArray = key.split("\\.");
+                String keyString = keyArray[keyArray.length - 1];
+
+                if (line.trim().startsWith(keyString + ":")) {
+
+                    if (config.isConfigurationSection(key)) {
+
+                        write(writer, line);
+                        continue outer;
+
+                    }
+
+                    String[] array = line.split(": ");
+
+                    if (array.length == 2) {
+
+                        if (array[1].startsWith("\"") || array[1].startsWith("'")) {
+
+                            char c = array[1].charAt(0);
+                            line = array[0] + ": " + c + config.get(key) + c;
+
+                        } else {
+
+                            line = array[0] + ": " + config.get(key);
+
+                        }
+
+                    }
+
+                    write(writer, line);
+                    continue outer;
+
+                }
+
+            }
+
+            write(writer, line);
+
+        }
+
+    }
+
+    private static BufferedReader getBufferedReader(InputStream inputStream) {
+
+        return new BufferedReader(new InputStreamReader(inputStream));
+
+    }
+
+    private static BufferedReader getBufferedReader(File file) throws FileNotFoundException {
+
+        return new BufferedReader(new FileReader(file));
+
+    }
+
+    private static void write(BufferedWriter writer, String line) throws IOException {
+
+        writer.write(line);
+        writer.newLine();
 
     }
 
