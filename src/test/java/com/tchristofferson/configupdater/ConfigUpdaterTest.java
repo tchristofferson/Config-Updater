7741c33957d5b08ddb15f0fbf91fdf5cc731f263
tchristofferson <longleadtrc@gmail.com>
2022-01-29 22:16:39 -0600

    Added test method to make sure ConfigUpdater deletes sections removed in default config

--- src/test/java/com/tchristofferson/configupdater/ConfigUpdaterTest.java
+++ src/test/java/com/tchristofferson/configupdater/ConfigUpdaterTest.java
@@ -4,7 +4,10 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.mockito.stubbing.Answer;
 
 import java.io.File;
@@ -18,22 +21,23 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyString;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ConfigUpdaterTest {
 
     private static final String FILE_NAME = "config.yml";
+    private static final String DELETE_SECTION_FILE_NAME = "test-delete-config.yml";
     private static final List<String> ignoredSections = Collections.singletonList("key6-ignored");
     private static Plugin plugin;
 
     @BeforeClass
     public static void beforeClass() {
         plugin = mock(Plugin.class);
-        when(plugin.getResource(anyString())).then((Answer<InputStream>) invocationOnMock -> ConfigUpdaterTest.class.getClassLoader().getResourceAsStream(FILE_NAME));
+        when(plugin.getResource(eq(FILE_NAME))).then((Answer<InputStream>) invocationOnMock -> ConfigUpdaterTest.class.getClassLoader().getResourceAsStream(FILE_NAME));
+        when(plugin.getResource(eq(DELETE_SECTION_FILE_NAME))).then((Answer<InputStream>) invocationOnMock -> ConfigUpdaterTest.class.getClassLoader().getResourceAsStream(DELETE_SECTION_FILE_NAME));
     }
 
     @AfterClass
@@ -75,6 +79,16 @@ public class ConfigUpdaterTest {
         assertTrue(config.contains("a-section-with-ignored-sections.sub-ignored.ignored2.value"));
         assertEquals(config.getInt("a-section-with-ignored-sections.sub-ignored.ignored.value3"), 3);
         assertEquals(config.getInt("a-section-with-ignored-sections.sub-ignored.ignored2.value"), 1);
+    }
+
+    @Test
+    public void testRemoveSection() throws IOException, InvalidConfigurationException {
+        File toUpdate = new File(FILE_NAME);
+        FileConfiguration config = YamlConfiguration.loadConfiguration(toUpdate);
+        assertTrue(config.contains("section2"));
+        ConfigUpdater.update(plugin, DELETE_SECTION_FILE_NAME, toUpdate, ignoredSections);
+        config = YamlConfiguration.loadConfiguration(toUpdate);//This works
+        assertFalse(config.contains("section2"));
     }
 
     private void saveDefaultConfig(File toUpdate) throws IOException, URISyntaxException {
