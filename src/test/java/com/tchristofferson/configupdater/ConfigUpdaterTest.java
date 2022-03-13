15531f482e1c28a2a4352b62ff460eb7521751d7
tchristofferson <longleadtrc@gmail.com>
2022-03-12 21:23:59 -0600

    Remove unused import

--- src/test/java/com/tchristofferson/configupdater/ConfigUpdaterTest.java
+++ src/test/java/com/tchristofferson/configupdater/ConfigUpdaterTest.java
@@ -19,7 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
